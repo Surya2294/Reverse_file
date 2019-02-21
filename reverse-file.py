36293ff2f33aeba5e9480dfdd494b51938171e5f
Your Name <surya.pandu315@gmail.com>
2019-02-21 16:08:32 +0530

    Initial commit

---
+++ reverse-file.py
@@ -0,0 +1,30 @@
+#!/usr/bin/ python
+
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(descripption='read a file in reverse')
+parser.add_argument('filename', help='the file to read')
+parser.add_argument('---limit', '-l', type=int, help='the number of lines to read')
+parser.add_argument('---version' '-v', action='version', version='%(prog)sversion 1.0')
+args = parser.parse_args()
+try:
+    f = open(args.filename)
+    limit = args.limit
+except FileNotFoundError as err:
+    print(f"Error : {err}")
+    sys.exit(1)
+else:
+    with f:
+        lines = f.readLine()
+        lines.reverse()
+
+        if limit:
+            lines = lines[:limit]
+
+        for line in lines:
+
+        print(line.strip()[::-1])
+
+
+
